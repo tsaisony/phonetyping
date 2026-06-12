c45c45b66ed940796afff2e49d9192bc2993e945
User <user@example.com>
2026-06-12 18:16:16 +0900

    refactor: replace tesseract with native iOS Live Text textarea

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import CameraView from './pages/CameraView';
-import OcrResult from './pages/OcrResult';
 import Practice from './pages/Practice';
 import './index.css';
 
@@ -15,7 +14,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/camera" element={<CameraView />} />
-            <Route path="/result" element={<OcrResult />} />
             <Route path="/practice/:id" element={<Practice />} />
           </Routes>
         </main>
